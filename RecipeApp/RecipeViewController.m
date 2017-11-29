c94b7430d398849a7139b74963aad6226e33a6cf
javx123 <javier.xing127@gmail.com>
2017-11-29 15:32:41 -0500

    Implmented saving recipies to persistanceStorage

--- RecipeApp/RecipeViewController.m
+++ RecipeApp/RecipeViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "RecipeViewController.h"
+#import "AppDelegate.h"
+
 
 
 
@@ -55,10 +57,34 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 - (IBAction)saveButtonTapped:(id)sender {
+    [self saveRecipe];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 
+-(void)saveRecipe{
+    AppDelegate *appDelegate = (AppDelegate*)[UIApplication sharedApplication].delegate;
+    Recipe *recipe = [[Recipe alloc]initWithContext:appDelegate.persistentContainer.viewContext];
+    
+    recipe.recipeName = self.selectedRecipe.recipeName;
+    recipe.recipeID = self.selectedRecipe.recipeID;
+    recipe.totalTime = self.selectedRecipe.totalTime;
+    
+    recipe.smallPictureURL = self.selectedRecipe.smallPictureURL;
+    recipe.largePictureURL = self.selectedRecipe.largePictureURL;
+    
+    recipe.ingredients = self.selectedRecipe.ingredients;
+    recipe.recipeURL = self.selectedRecipe.recipeURL;
+    recipe.rating = self.selectedRecipe.rating;
+    recipe.protein = self.selectedRecipe.protein;
+    recipe.calories = self.selectedRecipe.calories;
+    recipe.sugar = self.selectedRecipe.sugar;
+    recipe.fat = self.selectedRecipe.fat;
+    recipe.carbohydrate = self.selectedRecipe.carbs;
+    
+    [appDelegate.persistentContainer.viewContext insertObject:recipe];
+    [appDelegate saveContext];
+}
 
 
 
