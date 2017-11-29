cedee6aa3be5d9aaf9dfb4b8e6b1d550503deefe
javx123 <javier.xing127@gmail.com>
2017-11-29 16:11:19 -0500

    Saved recipe functionality

--- RecipeApp/ViewController.m
+++ RecipeApp/ViewController.m
@@ -7,35 +7,69 @@
 //
 
 #import "ViewController.h"
-#import "SearchViewController.h"
 #import "RecipeTableViewCell.h"
 #import "YummlyAPI.h"
 #import "Recipe+customInitializer.h"
+#import "AppDelegate.h"
+#import "SavedRecipesTableViewCell.h"
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UISearchControllerDelegate>
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 //@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (strong, nonatomic)UISearchController *searchController;
-@property (nonatomic, strong) SearchViewController *searchViewController;
-@property (nonatomic,strong) UINavigationController *navController;
-@property (nonatomic, strong)UISearchContainerViewController *searchContainer;
+@property (strong, nonatomic)NSArray <Recipe*> *savedRecipies;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self fetchRecipesData];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fetchRecipesData) name:NSManagedObjectContextDidSaveNotification object:nil];
+}
+
+-(void)fetchRecipesData{
+    AppDelegate *appDelegate = ((AppDelegate*)[[UIApplication sharedApplication] delegate]);
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Recipe"];
+//    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"tagName" ascending:YES];
+    self.savedRecipies = [appDelegate.persistentContainer.viewContext executeFetchRequest:request error:nil];
+}
+
+-(void)setSavedRecipies:(NSArray<Recipe *> *)savedRecipies{
+    _savedRecipies = savedRecipies;
+    [self.tableView reloadData];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return self.savedRecipies.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    RecipeTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"savedRecipeCell" forIndexPath:indexPath];
+    SavedRecipesTableViewCell *cell = (SavedRecipesTableViewCell*)[tableView dequeueReusableCellWithIdentifier:@"savedRecipeCell" forIndexPath:indexPath];
+    
+    Recipe *currentRecipe = self.savedRecipies[indexPath.row];
+    
+    cell.savedRecipeName.text = currentRecipe.recipeName;
+    
+    NSURL *smallImageURL = [NSURL URLWithString:currentRecipe.smallPictureURL];
+    NSData *smallImageData = [NSData dataWithContentsOfURL:smallImageURL];
+    UIImage *smallImage = [UIImage imageWithData:smallImageData];
+    cell.savedRecipeImageView.image = smallImage;
+    
+    cell.savedRecipeRating.text = [NSString stringWithFormat:@"Rating: %2.0f",currentRecipe.rating];
+    
+    if ([currentRecipe.totalTime isEqualToString:@"<null>"]) {
+        cell.savedRecipeTime.text = @"";
+    }
+    else{
+        int timeinSeconds = [currentRecipe.totalTime intValue];
+        int timeInMinutes = timeinSeconds/60;
+        NSString *timeString = [NSString stringWithFormat:@"%d",timeInMinutes];
+        cell.savedRecipeTime.text =  [NSString stringWithFormat:@"%@ Minutes",timeString];
+    }
     
     return cell;
-
 }
 
 
