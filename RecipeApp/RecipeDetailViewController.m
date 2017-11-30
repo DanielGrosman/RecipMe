db3c9e179a699ee6aec3414bf1f0bd38b77728b9
Daniel Grosman <danny.grosman@gmail.com>
2017-11-30 12:03:14 -0500

    formatted detail view for saved recipes

--- RecipeApp/RecipeDetailViewController.m
+++ RecipeApp/RecipeDetailViewController.m
@@ -8,7 +8,7 @@
 
 #import "RecipeDetailViewController.h"
 
-@interface RecipeDetailViewController ()
+@interface RecipeDetailViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *savedDetailImageView;
 @property (weak, nonatomic) IBOutlet UILabel *savedDetailName;
 @property (weak, nonatomic) IBOutlet UILabel *savedDetailIngredients;
@@ -17,6 +17,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *savedDetailProtein;
 @property (weak, nonatomic) IBOutlet UILabel *savedDetailSugar;
 @property (weak, nonatomic) IBOutlet UILabel *savedDetailCarbs;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *labelBackgroundView;
 
 @end
 
@@ -24,12 +26,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.labelBackgroundView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5f];
     [self setupRecipeView];
 }
 
 -(void)setupRecipeView{
     self.savedDetailName.text = self.recipe.recipeName;
-    self.savedDetailIngredients.text = self.recipe.ingredients;
+//    self.savedDetailIngredients.text = self.recipe.ingredients;
+    self.ingredientsArray = [self.recipe.ingredients componentsSeparatedByString:@", "];
     self.savedDetailCalories.text = [NSString stringWithFormat:@"%2.0f", self.recipe.calories];
     self.savedDetailProtein.text = [NSString stringWithFormat:@"%2.0f", self.recipe.protein];
     self.savedDetailSugar.text = [NSString stringWithFormat:@"%2.0f", self.recipe.sugar];
@@ -46,8 +50,32 @@
         self.savedDetailImageView.image = largeImage;
         self.savedDetailImageView.contentMode = UIViewContentModeScaleAspectFill;
     }
+    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        [self.tableView reloadData];
+    }];
     
 }
+
+#pragma mark UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.ingredientsArray.count;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.textLabel.text = self.ingredientsArray[indexPath.row];
+    
+    return cell;
+}
+
+
 
 
 @end
