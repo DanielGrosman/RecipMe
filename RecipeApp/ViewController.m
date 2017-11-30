0e6f58a0a296ea9c5a33925d3c00d2d5bc5d0fe5
javx123 <javier.xing127@gmail.com>
2017-11-30 14:29:38 -0500

    Added delete functionality

--- RecipeApp/ViewController.m
+++ RecipeApp/ViewController.m
@@ -14,11 +14,13 @@
 #import "SavedRecipesTableViewCell.h"
 #import "RecipeDetailViewController.h"
 #import <XLPagerTabStrip/XLPagerTabStripViewController.h>
+#import <CoreData/CoreData.h>
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource,XLPagerTabStripChildItem>
-//@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource,XLPagerTabStripChildItem,NSFetchedResultsControllerDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic)NSArray <Recipe*> *savedRecipies;
+@property (strong, nonatomic)NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (weak, nonatomic) IBOutlet UIButton *editButton;
 
 @end
@@ -27,27 +29,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-        self.editButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75f];
     
     self.tableView.contentInset = UIEdgeInsetsMake(44, 0, 0, 0);
-//    [self.tableView setContentOffset:CGPointMake(0, 44)];
-    
     [self fetchRecipesData];
-    NSLog(@"%@", @(self.tableView.contentInset.top));
-    
+    self.editButton.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75f];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fetchRecipesData) name:NSManagedObjectContextDidSaveNotification object:nil];
 }
 
-- (void)viewDidLayoutSubviews {
-    NSLog(@"%@", NSStringFromCGPoint(self.tableView.contentOffset));
-
-}
+#pragma mark - Setup View
 
 -(void)fetchRecipesData{
     AppDelegate *appDelegate = ((AppDelegate*)[[UIApplication sharedApplication] delegate]);
     NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Recipe"];
 //    NSSortDescriptor *sort = [NSSortDescriptor sortDescriptorWithKey:@"tagName" ascending:YES];
+    self.managedObjectContext = appDelegate.persistentContainer.viewContext;
     self.savedRecipies = [appDelegate.persistentContainer.viewContext executeFetchRequest:request error:nil];
 }
 
@@ -55,6 +51,8 @@
     _savedRecipies = savedRecipies;
     [self.tableView reloadData];
 }
+
+#pragma mark - TableView DataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.savedRecipies.count;
@@ -66,11 +64,7 @@
     Recipe *currentRecipe = self.savedRecipies[indexPath.row];
     
     cell.savedRecipeName.text = currentRecipe.recipeName;
-    
-//    NSURL *smallImageURL = [NSURL URLWithString:currentRecipe.smallPictureURL];
-//    NSData *smallImageData = [NSData dataWithContentsOfURL:smallImageURL];
-//    UIImage *smallImage = [UIImage imageWithData:smallImageData];
-//    cell.savedRecipeImageView.image = smallImage;
+
     NSArray *docDirectory = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *imageFilePath = [docDirectory firstObject];
     imageFilePath = [imageFilePath stringByAppendingString:currentRecipe.largeImagePath];
@@ -97,6 +91,65 @@
     return cell;
 }
 
+#pragma mark - Editing TableView
+
+- (IBAction)edit:(id)sender {
+    if (self.tableView.isEditing ) {
+        [self.tableView setEditing:NO animated:YES];
+    }
+    else{
+    [self.tableView setEditing:YES animated:YES];
+    }
+}
+
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+    return  YES;
+}
+
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+        [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
+        
+        NSError *error = nil;
+        if (![context save:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+
+    }
+}
+
+#pragma mark - FetchResultsController
+
+-(NSFetchedResultsController *)fetchedResultsController{
+    if (_fetchedResultsController != nil) {
+        return _fetchedResultsController;
+    }
+    NSFetchRequest *fetchRequest = [Recipe fetchRequest];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc]initWithKey:@"rating" ascending:YES];
+    [fetchRequest setSortDescriptors:@[sortDescriptor]];
+    
+    self.fetchedResultsController = [[NSFetchedResultsController alloc]initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+    self.fetchedResultsController.delegate = self;
+    
+    NSError *error = nil;
+    if (![self.fetchedResultsController performFetch:&error]) {
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    return _fetchedResultsController;
+}
+
+#pragma mark - Segue
+
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     if ([segue.identifier isEqualToString:@"savedRecipeDetails"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
@@ -107,6 +160,8 @@
         [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
     }
 }
+
+#pragma mark -Custom TabBar 
 
 - (NSString *)titleForPagerTabStripViewController:(XLPagerTabStripViewController *)pagerTabStripViewController{
     return @"Saved Recipes";
